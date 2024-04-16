545cf070a418388dc91afd09013f3202b2728480
Leonel Medina <123245262+leomed98@users.noreply.github.com>
2024-04-15 22:13:49 -0700

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
 
-
 int main(){
 
+    char op; //operator
+    double num1;
+    double num2;
+    double result;
 
+
+std::cout << "--------------CALCULATOR--------------\n";
+
+std::cout << "Enter either (+-*/) please: ";
+std::cin >> op;
+
+std::cout <<"Enter #1";
+std::cin>> num1;
+
+std::cout<< "Enter #2";
+std::cin >> num2;
+
+switch(op){
+    case '+':
+    result= num1+num2;
+    std::cout<< "Result!: " << result << '\n';
+    break; 
+
+    case '-':
+    result=num1-num2;
+    std::cout << "Result!: " << result << '\n';
+    break;
+
+    case '*':
+    result=num1*num2;
+    std::cout << "Result!: " << result << '\n';
+    break;
+
+    case '/':
+    result= num1/num2;
+    std::cout << "Result!: " << result << '\n';
+    break;
+
+    default:
+    std::cout<< "Invalid Respone! \n";
+    break;
+
+}
+
+
+std::cout << "--------------CALCULATOR--------------\n";
+
+
+
+
+    return 0;
 
 }
